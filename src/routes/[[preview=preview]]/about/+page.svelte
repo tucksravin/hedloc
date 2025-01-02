4ccc3bf1ccc633840d9533eb907098ce6eb68155
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2025-01-02 11:09:34 -0500

    responsive changes

--- src/routes/[[preview=preview]]/about/+page.svelte
+++ src/routes/[[preview=preview]]/about/+page.svelte
@@ -14,6 +14,7 @@
 
     let active = $state(-1);
 
+    $effect(()=>console.log(active))
 
     let submit = () => {if(form)form.submit()};
     let form:HTMLFormElement|undefined = $state();
@@ -32,25 +33,25 @@
     <ContentWidth class="text-white mb-10">
         <h2>{content.team_header}</h2>
     </ContentWidth>
-    <ContentWidth class="flex flex-col lg:flex-row justify-end relative lg:h-[600px] gap-10" >
-        <div class="lg:w-1/4 flex flex-col items-start justify-start gap-2 z-20" id="team-top">
-            {#if active>-1}
-            <button transition:fade={{duration:150}} onclick={()=>active=-1} class="hidden lg:flex flex-col items-start justify-start gap-2 text-white">
-                <PrismicImage class="w-full aspect-[3/4]" field={content.team_member[active].headshot||''} />
-                <h4>{content.team_member[active].name}</h4>
-                <h5>{content.team_member[active].title}</h5>
-            
-                <div class="mt-8 button-text transition w-36 h-9 border-2 border-white text-white hover:bg-white active:bg-dark active:text-white hover:text-dark flex items-center justify-center">Close</div>
-            
+    <ContentWidth class="flex flex-col lg:flex-row justify-start relative lg:h-[640px] gap-10" >
+        <div class="lg:w-1/4 flex flex-col items-start justify-start gap-2 z-30" id="team-top">
+            {#if active!==-1}
+                <button transition:fade={{duration:150}} onclick={()=>active=-1} class="hidden lg:flex flex-col items-start justify-start gap-2 text-white">
+                    <PrismicImage class="w-full aspect-[3/4]" field={content.team_member[active].headshot} />
+                    <h4>{content.team_member[active].name}</h4>
+                    <h5>{content.team_member[active].title}</h5>
+                
+                    <div class="mt-8 button-text transition w-36 h-9 border-2 border-white text-white hover:bg-white active:bg-dark active:text-white hover:text-dark flex items-center justify-center">Close</div>
+                
 
-            </button>
+                </button>
             {/if}
             
             
         </div>
         {#each content.team_member as p, i}
         <button 
-            onclick={()=>{if(active===i){ active=-1}else{active=i; document.getElementById('team-top')?.scrollIntoView({behavior:'smooth', block: 'start'})}}} 
+            onclick={()=>{if(active===i){ active=-1}else{active=i; if(viewportWidth<1024){document.getElementById('team-top')?.scrollIntoView({behavior:'smooth', block: 'start'})}}}} 
             class="lg:w-1/4 members flex flex-col items-start justify-start gap-2 {active!==-1?"pointer-events-none":""}">
             <PrismicImage class="w-full aspect-[3/4] object-cover mb-4" field={content.team_member[i].headshot} />
             <h4>{content.team_member[i].name}</h4>
@@ -61,8 +62,8 @@
         {/each}
         <div class="w-full h-full absolute bg-light flex flex-col justify-start items-end z-20 pointer-events-none transition-opacity duration-150 {active===-1?"opacity-0":""}">
             {#if active>-1}
-                <div class="text-white lg:w-2/3" in:fade={{duration:150, delay: 150}} out:fade={{duration:150}}>
-                    <button transition:fade={{duration:150}} onclick={()=>active=-1} class="flex flex-col items-start justify-start gap-2 text-white">
+                <div class=" text-white lg:w-2/3" in:fade={{duration:150, delay: 150}} out:fade={{duration:150}}>
+                    <button transition:fade={{duration:150}} onclick={()=>active=-1} class="flex lg:hidden flex-col items-start justify-start gap-2 text-white">
                         <PrismicImage class="w-full aspect-[3/4]" field={content.team_member[active].headshot||''} />
                         <h4>{content.team_member[active].name}</h4>
                         <h5>{content.team_member[active].title}</h5>
@@ -70,10 +71,10 @@
                         
         
                     </button>
-                    <div class="mt-8">
+                    <div class="lg:mt-8">
                         <PrismicRichText  field={content.team_member[active].body||''} />
                     </div>
-                    <button onclick={()=>{document.getElementsByClassName('members')[active].scrollIntoView({behavior:'smooth', block: 'start'}); active=-1;}} class="pointer-events-auto mt-8 button-text transition w-36 h-9 border-2 border-white text-white hover:bg-white active:bg-dark active:text-white hover:text-dark flex items-center justify-center">Close</button>
+                    <button onclick={()=>{document.getElementsByClassName('members')[active].scrollIntoView({behavior:'smooth', block: 'start'}); active=-1;}} class=" lg:hidden pointer-events-auto mt-8 button-text transition w-36 h-9 border-2 border-white text-white hover:bg-white active:bg-dark active:text-white hover:text-dark flex items-center justify-center">Close</button>
                     
                 </div>
                 
